53398c9268e003dc43e697525d31991561e39f46
Kyle <makkoli@vt.edu>
2016-10-12 17:07:23 +0000

    finished parser logic for headers

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+var ip = "x-forwarded-for";
+var language = "accept-language";
+var software = "user-agent";
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.status(200);
+    // Send the parsed header object in the response
+    res.send(
+        {
+            "ip address": req.headers[ip],
+            "language": req.headers[language].match(/(.*),/)[1],
+            "software": req.headers[software].match(/\((.*)\)/)[1]
+        }
+    );
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(8080);
